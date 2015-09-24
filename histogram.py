141c472b417d07f7ae8f5279f729d0691b23e5ff
Isabella Cai <isabella.cai@students.makeschool.com>
2015-09-24 16:59:07 -0700

    there is now a total count of all the unique words in the file

--- histogram.py
+++ histogram.py
@@ -1,10 +1,6 @@
 import collections
-# input_file = open("shirley.txt")
-#
-# words = []
-# for line in input_file:
-#     words.append(word)
-#
+
+
 # def histogram(arg):
 #     pass
 #
@@ -14,22 +10,18 @@ import collections
 # def frequency(word, histogram):
 #     pass
 
-
 file = open('shirley.txt', 'r')
 text = [word.replace(",", "").replace(".", "").replace(";", "")
         .replace(":", "") for line in file for word in line.lower().split()]
 
 
-print(text)
 c = collections.Counter()
-# with open('shirley.txt', 'r') as f:
-#     for line in f:
-#         for word in line.lower().line.strip(',').split():
-#             word.replace(".", "")
-#             words.append(word)
-# print(words)
 
 for a in text:
     c[a] += 1
 
 print(c)
+
+uniquewords = len(set(w.lower() for w in text))
+
+print(uniquewords)
